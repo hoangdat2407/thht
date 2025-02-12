c41add37774e689a96d6b7899608efed6199807e
datjava <23021518@vnu.edu.vn>
2025-02-12 10:41:47 +0700

    2

---
+++ gen.py
@@ -0,0 +1,7 @@
+import os
+
+folder_path = "."  # Thay bằng đường dẫn thư mục của bạn
+file_names = os.listdir(folder_path)
+
+for file in file_names:
+    print(f'<a href="{file}">{file}</a>')  # Dùng f-string để format chuỗi
